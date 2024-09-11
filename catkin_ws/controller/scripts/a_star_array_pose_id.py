3f327ee4812d1936ad3e726966f17ded9d8be0d1
Clara <oi.mariaclaraalves@gmail.com>
2024-09-11 08:29:58 -0300

    navigator e a_star funcionando

--- catkin_ws/controller/scripts/a_star_array_pose_id.py
+++ catkin_ws/controller/scripts/a_star_array_pose_id.py
@@ -3,52 +3,37 @@
 import rospy
 import heapq
 import math
-from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
+from geometry_msgs.msg import Pose, Point, Quaternion
 from std_msgs.msg import Header
+from system.msg import PoseArrayWithID  
 import time
 
 last_pose = Pose()
+contador = 0  # Adicionando um contador global para o ID
 
-#converter de Pose para coordenadas (x, y, z)
+# converter de Pose para coordenadas (x, y, z)
 def pose_para_coordenadas(pose):
     return (pose.position.x, pose.position.y, pose.position.z)
 
-#converter de coordenadas (x, y, z) para Pose
+# converter de coordenadas (x, y, z) para Pose
 def coordenadas_para_pose(x, y, z):
     return Pose(position=Point(x, y, z), orientation=Quaternion(0, 0, 0, 1))
 
-
-def coordenadas_para_posestamped(x, y, z, frame_id='map', stamp=None):
-    pose = Pose(position=Point(x, y, z), orientation=Quaternion(0, 0, 0, 1))
-    
-    pose_stamped = PoseStamped()
-    pose_stamped.header = Header(frame_id=frame_id, stamp=stamp)
-    pose_stamped.pose = pose
-    
-    return pose_stamped
-
-#global de obstáculos (como coordenadas para hash)
+# global de obstáculos (como coordenadas para hash)
 obstaculos = [
     pose_para_coordenadas(Pose(position=Point(1, 1, 1), orientation=Quaternion(0, 0, 0, 1))),
     pose_para_coordenadas(Pose(position=Point(3, 2, 0.5), orientation=Quaternion(0, 0, 0, 1))),
-   # pose_para_coordenadas(Pose(position=Point(7, 2, 1), orientation=Quaternion(0, 0, 0, 1)))
+    # pose_para_coordenadas(Pose(position=Point(7, 2, 1), orientation=Quaternion(0, 0, 0, 1)))
 ]
 
-#calcular a heurística (distância euclidiana) entre dois nós (como coordenadas)
+# calcular a heurística (distância euclidiana) entre dois nós (como coordenadas)
 def heuristica_euclidiana(no_atual, no_objetivo):
     dx = no_atual[0] - no_objetivo[0]
     dy = no_atual[1] - no_objetivo[1]
     dz = no_atual[2] - no_objetivo[2]
     return math.sqrt(dx**2 + dy**2 + dz**2)
 
-# calcular a heurística (distância de Manhattan) entre dois nós (como coordenadas)
-def heuristica_manhattan(no_atual, no_objetivo):
-    dx = abs(no_atual[0] - no_objetivo[0])
-    dy = abs(no_atual[1] - no_objetivo[1])
-    dz = abs(no_atual[2] - no_objetivo[2])
-    return dx + dy + dz
-
-#algoritmo A*
+# algoritmo A*
 def a_estrela(inicio, objetivo, vizinhos_funcao):
     aberta = []
     fechada = set(obstaculos)
@@ -62,7 +47,7 @@ def a_estrela(inicio, objetivo, vizinhos_funcao):
             caminho_final = []
             while no_atual:
                 caminho_final.append(no_atual)
-                no_atual = caminho[no_atual]
+                no_atual = caminho[caminho_final[-1]]
             return [coordenadas_para_pose(x, y, z) for (x, y, z) in caminho_final[::-1]]
 
         fechada.add(no_atual)
@@ -78,7 +63,7 @@ def a_estrela(inicio, objetivo, vizinhos_funcao):
 
     return None
 
-#obter os vizinhos de um nó (como coordenadas)
+# obter os vizinhos de um nó (como coordenadas)
 def obter_vizinhos(no):
     x, y, z = no
     return [
@@ -87,7 +72,7 @@ def obter_vizinhos(no):
         (x, y, z + 1), (x, y, z - 1)
     ]
 
-#para nós-alvo
+# para nós-alvo
 def calcular_trajetorias(nos_alvo, vizinhos_funcao):
     trajetorias = []
     lista_continua = []
@@ -111,24 +96,30 @@ def calcular_trajetorias(nos_alvo, vizinhos_funcao):
 
         tempo_total += tempo_processamento
 
-    ##converte a lista contínua de coordenadas para uma lista de Poses
-    lista_continua_poses = [coordenadas_para_posestamped(x, y, z) for (x, y, z) in lista_continua]
+    # converte a lista contínua de coordenadas para uma lista de Poses
+    lista_continua_poses = [coordenadas_para_pose(x, y, z) for (x, y, z) in lista_continua]
 
     return trajetorias, lista_continua_poses, tempo_total, tamanho_total
 
-def publish_pose_list(pose_list):
+def publish_pose_array(contador, pose_array):
     if not rospy.get_node_uri():
-        rospy.init_node('pose_publisher', anonymous=True)
+        rospy.init_node('pose_array_publisher', anonymous=True)
     
-    pub = rospy.Publisher('pose_topic', PoseStamped, queue_size=10)
+    pub = rospy.Publisher('pose_array_topic', PoseArrayWithID, queue_size=10)
     rate = rospy.Rate(1)  # 1 Hz
 
-    for pose in pose_list:
+    while not rospy.is_shutdown():
+        pose_array_with_id_msg = PoseArrayWithID()
+        pose_array_with_id_msg.header = Header(frame_id='map', stamp=rospy.Time.now())
+        pose_array_with_id_msg.id = str(contador)  # Convertendo o ID para string
+        pose_array_with_id_msg.poses = pose_array
+
         try:
-            pub.publish(pose)
-            rospy.loginfo("Publicado: %s", pose)
+            pub.publish(pose_array_with_id_msg)
+            rospy.loginfo("Publicado: %d poses com ID %s.", len(pose_array), pose_array_with_id_msg.id)
         except rospy.ROSInterruptException as e:
             rospy.logerr("Erro ao publicar: %s", e)
+
         rate.sleep()
 
 if __name__ == '__main__':
@@ -140,14 +131,12 @@ if __name__ == '__main__':
             coordenadas_para_pose(0, 0, 0),
             coordenadas_para_pose(3, 3, 3),
             coordenadas_para_pose(4, 5, 6),
-            coordenadas_para_pose(5, 7, 6),  
+            coordenadas_para_pose(2, 4, 5),  
             initial_pose
         ]
 
         trajetorias, lista_continua, tempo_total, tamanho_total = calcular_trajetorias(nos_alvo, obter_vizinhos)
-
-        publish_pose_list(lista_continua)
-
+        
         print("Trajetórias encontradas:")
         for i, (trajeto, tamanho, tempo) in enumerate(trajetorias):
             if trajeto:
@@ -157,9 +146,12 @@ if __name__ == '__main__':
                 print(f"Trajetória {i + 1}: Nenhum caminho encontrado")
             print(f"Tempo de processamento: {tempo:.4f} segundos\n")
 
-        print(f"Lista contínua de coordenadas: {lista_continua}\n")
+        #print(f"Lista contínua de coordenadas: {lista_continua}\n")
         print(f"Tamanho total da trajetória contínua: {len(lista_continua)} nós")
         print(f"Tempo total de processamento: {tempo_total:.4f} segundos\n")
+
+        contador+=1
+        publish_pose_array(contador, lista_continua)
 
     except rospy.ROSInterruptException:
         pass
--- catkin_ws/controller/scripts/navigator_w_id.py
+++ catkin_ws/controller/scripts/navigator_w_id.py
@@ -2,16 +2,30 @@
 
 import rospy
 from geometry_msgs.msg import Pose, Quaternion, Point, PoseStamped, Vector3
+from system.msg import PoseArrayWithID
 
 last_pose = Pose()
+
+pose_list = []  # Lista de poses para navegar
+current_id = None  # ID do último conjunto de poses processado
+
+current_index = 0
+
 scale = 1.0
 
 def callback_current_pose(data):
-  global last_pose
-  
-  # get the last pose
-  last_pose = data.pose
-  
+    global last_pose
+    # Atualiza a última pose recebida
+    last_pose = data.pose
+
+def callback_pose_array(data):
+    global pose_list, current_id
+    # Verifica se o ID recebido é diferente do ID atual
+    if data.id != current_id:
+        # Atualiza o ID atual e a lista de poses
+        current_id = data.id
+        pose_list.extend(data.poses)
+        rospy.loginfo("Atualizado com novas poses. ID: %s", current_id)
 
 def update_target(base_pose: Pose, target_pose: Pose) -> bool:
     diferenca = Vector3()
@@ -25,9 +39,9 @@ def update_target(base_pose: Pose, target_pose: Pose) -> bool:
     limiar_altura = 0.5
     limiar = 1.0
 
-    rospy.loginfo("Base pose: %s %s %s", base_pose.position.z, base_pose.position.x, base_pose.position.y)
+    #rospy.loginfo("Base pose: %s %s %s", base_pose.position.z, base_pose.position.x, base_pose.position.y)
     rospy.loginfo("Target: %s %s %s", target_pose.position.z, target_pose.position.x, target_pose.position.y)
-    rospy.loginfo("Diferença: %s %s %s", diferenca.z, diferenca.x, diferenca.y)
+    #rospy.loginfo("Diferença: %s %s %s", diferenca.z, diferenca.x, diferenca.y)
 
     # Todos os valores próximos ao limiar
     if abs(diferenca.z)<=limiar_altura and abs(diferenca.x)<=limiar and abs(diferenca.y)<=limiar:
@@ -35,25 +49,36 @@ def update_target(base_pose: Pose, target_pose: Pose) -> bool:
 
     return False
 
-def publish_pose_list(pose_list):
+def publish_pose_list():
+    global pose_list, current_id, current_index  # Declare todas as variáveis globais que você pretende modificar
+
     rospy.init_node("pose_navigator", anonymous=True)
 
+    # Assina os tópicos necessários
     rospy.Subscriber("/ground_truth_to_tf/pose", PoseStamped, callback_current_pose)
+    rospy.Subscriber("pose_array_topic", PoseArrayWithID, callback_pose_array)
 
     pub = rospy.Publisher("/drone/target", Pose, queue_size=10)
 
-    rate = rospy.Rate(10) 
+    rate = rospy.Rate(10)  # 10 Hz
 
-    current_index = 0
     while not rospy.is_shutdown():
         # Verifica se ainda há poses na lista
         if current_index >= len(pose_list):
             rospy.loginfo("Todas as poses da lista foram alcançadas.")
-            break
+            continue  # Volta para o início da lista
 
+        if not pose_list:
+            # Se a lista de poses estiver vazia, aguarde novas poses
+            rospy.loginfo("Aguardando novas poses...")
+            rate.sleep()
+            continue
+        
         target_pose = pose_list[current_index]
         # Verifica se a pose deve ser atualizada
         if update_target(last_pose, target_pose):
+            rospy.loginfo("%d", current_index)
+            rospy.loginfo("Last pose: (%f, %f, %f)", last_pose.position.x, last_pose.position.y, last_pose.position.z)
             rospy.loginfo("Atualizando pose...")
             current_index += 1
             continue
@@ -61,17 +86,9 @@ def publish_pose_list(pose_list):
         pub.publish(target_pose)
         rate.sleep()
 
+
 if __name__ == '__main__':
-    initial_pose = last_pose
-
-    pose_list = [
-        Pose(position=Point(x=1.0, y=2.0, z=5.0), orientation=Quaternion(x=0.0, y=0.0, z=0.0, w=0.0)),
-        Pose(position=Point(x=2.0, y=3.0, z=5.0), orientation=Quaternion(x=0.0, y=0.0, z=0.0, w=0.0)),
-        Pose(position=Point(x=3.0, y=4.0, z=1.0), orientation=Quaternion(x=0.0, y=0.0, z=0.0, w=0.0)),
-        initial_pose,
-    ]
-
     try:
-        publish_pose_list(pose_list)
+        publish_pose_list()
     except rospy.ROSInterruptException:
         pass
